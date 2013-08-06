eb5a09727bcdb4939cc7f7c2341d04abf3d5a3dc
jgrimaldos <jgrimaldos@gmail.com>
2013-08-06 15:17:52 -0500

    Heroku REST Test

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-#!flask/bin/python
-from app import app
-app.run(debug = True)
+from flask import Flask, request, render_template, session
+app = Flask(__name__)
+app.secret_key = 'u\x91\xa1r#\x9e\xea\xd1\xb2D\x88pG\x98\x17\xe0'
+
+@app.route('/')
+@app.route('/oauth2/callback')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/auth', methods=['POST'])
+def authorized():
+	session['auth_id'] = request.form['auth']
+	return render_template('auth.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
